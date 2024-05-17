6a0a317b334b9bfe7cfdf57abdfc4fa4223674ad
PatBiz <103768365+PatBiz@users.noreply.github.com>
2024-05-17 14:06:34 +0200

    Patch Player (keyboard version)

--- src/player_keyboard.ts
+++ src/player_keyboard.ts
@@ -1,5 +1,5 @@
 //# Third-party :
-import type { Scene, AbstractMesh, Skeleton, AnimationGroup } from "@babylonjs/core";
+import type { Scene, AbstractMesh, Skeleton } from "@babylonjs/core";
 import { SceneLoader } from "@babylonjs/core";
 
 
@@ -11,43 +11,22 @@ type PlayerModel = {
   skeleton: Skeleton
 };
 
-type PlayerAnimationDict = { [key: string]: AnimationGroup };
-
 export class Player {
+  public constructor(private scene: Scene){}
+
   //# ¤¤¤¤¤¤¤¤¤¤¤¤ ATTRIBUTES ¤¤¤¤¤¤¤¤¤¤¤¤ #//
   private _model?: PlayerModel;
-  private _animations : PlayerAnimationDict;
-
-
-  //# ¤¤¤¤¤¤¤¤¤¤¤ CONSTRUCTORS ¤¤¤¤¤¤¤¤¤¤¤ #//
-  public constructor(private scene: Scene){
-    this._animations = {
-      "climb": this.scene.getAnimationGroupByName("climb")!,
-      "hang": this.scene.getAnimationGroupByName("hang")!,
-      "hop_left": this.scene.getAnimationGroupByName("hop_left")!,
-      "hop_right": this.scene.getAnimationGroupByName("hop_right")!,
-      "shimmy_left": this.scene.getAnimationGroupByName("shimmy_left")!,
-      "shimmy_right": this.scene.getAnimationGroupByName("shimmy_right")!
-    }
-  }
 
 
   //# ¤¤¤¤¤¤¤¤¤¤¤¤¤¤ GETTERS ¤¤¤¤¤¤¤¤¤¤¤¤¤ #//
   public get model(): PlayerModel{return this._model!}
-  public get animations(): PlayerAnimationDict {return this._animations}
+
 
   //# ¤¤¤¤¤¤¤¤¤¤¤¤¤¤ METHODS ¤¤¤¤¤¤¤¤¤¤¤¤¤ #//
-
-  //********* Player Initialisation ********//
-
   public async importPlayerModel() {
     const {meshes, skeletons} = await SceneLoader.ImportMeshAsync(
       "", "../assets/models/", "climber.glb", this.scene
     );
     this._model = {mesh: meshes[0], skeleton: skeletons[0]};
   }
-
-  //************* Player Actions ***********//
-
-  //...
 }
